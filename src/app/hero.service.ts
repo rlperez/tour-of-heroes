b24b617b046bff6548e7b0162b63a81e6b204d27
rlperez <rlperez@udel.edu>
2019-12-08 19:25:19 -0500

    Make getHeroes async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+
 }
